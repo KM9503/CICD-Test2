5a8e8e227c9eb23057a372e5326fe0d494e4f2dc
KM903 <kailasmore079@gmail.com>
2023-12-04 08:50:04 +0530

    Added selenium Grid code

--- src/test/java/AutomationProject/LaunchClass.java
+++ src/test/java/AutomationProject/LaunchClass.java
@@ -1,22 +1,33 @@
 package AutomationProject;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class LaunchClass {
 
-    public static WebDriver driver = null;
+    public static WebDriver driver;
 
     @BeforeMethod
-    public void LaunchDriver() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+    public void LaunchDriver() throws MalformedURLException {
+        DesiredCapabilities dr = null;
+        dr = DesiredCapabilities.chrome();
+        dr.setBrowserName("chrome");
+        dr.setPlatform(Platform.ANY);
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Kailas More\\Desktop\\Chrome driver\\chromedriver-win32\\chromedriver-win32\\chromedriver.exe");
+//        WebDriverManager.chromedriver().setup();
+//        driver = new ChromeDriver();
+        driver = new RemoteWebDriver(new URL("http://192.168.84.1:4444/wd/hub"), dr);
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.manage().window().fullscreen();
     }
